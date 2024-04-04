34e57ebbecf982fd6ace7cf566ab7becca19ec2e
vahid karimi <149614062+vahidKarimiDev@users.noreply.github.com>
2024-04-04 10:59:14 +0330

    add components Singel Card

---
+++ src/Components/SingelCards/SingelCards.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./SingelCards.css";
+
+const SingelCards = ({ card, handelChoice, flipped }) => {
+  const handelClick = () => {
+    handelChoice(card);
+  };
+
+  return (
+    <>
+      <div className="card" key={card.id}>
+        <div className={flipped ? "flipped" : ""}>
+          <img className="front" src={card.src} alt="image" />
+          <img
+            className="back"
+            src="cover.jpg"
+            alt="cover"
+            onClick={handelClick}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingelCards;
